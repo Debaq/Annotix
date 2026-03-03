78d0b07952aa23d559a7a005a3a74a2c49e3570c
Nicolás Baier Quezada <15371725+Debaq@users.noreply.github.com>
2026-03-03 13:41:54 -0300

    Release Annotix v2.0.2 (jfxto)

--- src/lib/buildInfo.ts
+++ src/lib/buildInfo.ts
@@ -1,4 +1,4 @@
 // Auto-generado por annotix.sh — no editar manualmente
-export const BUILD_NUMBER = 17;
-export const BUILD_CODE = "gjibw";
-export const BUILD_DATE = "2026-03-03T16:26:14Z";
+export const BUILD_NUMBER = 19;
+export const BUILD_CODE = "hvwsz";
+export const BUILD_DATE = "2026-03-03T16:30:55Z";
