74a308568ee5c13af73a87c927e943c1f555310f
Nicolás Baier Quezada <15371725+Debaq@users.noreply.github.com>
2026-03-03 11:14:22 -0300

    Release Annotix v2.0.2 (cdiaw)

--- src/lib/buildInfo.ts
+++ src/lib/buildInfo.ts
@@ -1,4 +1,4 @@
 // Auto-generado por annotix.sh — no editar manualmente
-export const BUILD_NUMBER = 14;
-export const BUILD_CODE = "zvvjf";
-export const BUILD_DATE = "2026-03-03T14:11:17Z";
+export const BUILD_NUMBER = 15;
+export const BUILD_CODE = "cdiaw";
+export const BUILD_DATE = "2026-03-03T14:14:17Z";
