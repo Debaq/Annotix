61285b24bb1f1ec1a1c8ccd609c5dedc2bbc94f8
Nicolás Baier Quezada <15371725+Debaq@users.noreply.github.com>
2026-03-03 10:20:02 -0300

    Release Annotix v2.0.1 (ztjex)

--- src/lib/buildInfo.ts
+++ src/lib/buildInfo.ts
@@ -1,4 +1,4 @@
 // Auto-generado por annotix.sh — no editar manualmente
-export const BUILD_NUMBER = 6;
-export const BUILD_CODE = "eqlfz";
-export const BUILD_DATE = "2026-03-03T12:46:50Z";
+export const BUILD_NUMBER = 7;
+export const BUILD_CODE = "ztjex";
+export const BUILD_DATE = "2026-03-03T13:19:43Z";
